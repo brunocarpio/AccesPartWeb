14858889fb6e02b78f5e532a02059ce77f1e3927
Bruno Carpio <bcarpio181@outlook.es>
2021-05-07 08:31:27 -0500

    basic template ProductModal add

--- src/components/ProductModal.js
+++ src/components/ProductModal.js
@@ -8,19 +8,41 @@ import {
   ModalBody,
   ModalCloseButton,
   Image,
+  useDisclosure,
+  Button,
 } from "@chakra-ui/react"
 
 function ProductModal(props) {
-  return (
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-    <Image
-      rounded={'lg'}
-      height={'230'}
-      width={'282'}
-      objectFit={'cover'}
-      src={props.product.media.source}
-      cursor={'pointer'}
-    />
+  return (
+    <>
+      <Image
+        rounded={'lg'}
+        height={'230'}
+        width={'282'}
+        objectFit={'cover'}
+        src={props.product.media.source}
+        cursor={'pointer'}
+        onClick={onOpen}
+      />
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
